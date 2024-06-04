99951b3320eb7b9270ac788cfd547704f0acada9
leonelborgesdev <leonel03t@gmail.com>
2024-06-04 11:09:42 -0400

    data

---
+++ src/data/videogames.js
@@ -0,0 +1,81 @@
+export default {
+  count: 868273,
+  next: "https://api.rawg.io/api/games?key=fcbeecdc2f4c4b3e90e904fa4113f198&page=2",
+  previous: null,
+  results: [
+    {
+      id: 3498,
+      slug: "grand-theft-auto-v",
+      name: "Grand Theft Auto V",
+      released: "2013-09-17",
+      tba: false,
+      background_image:
+        "https://media.rawg.io/media/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg",
+      rating: 4.47,
+      rating_top: 5,
+      ratings: [
+        {
+          id: 5,
+          title: "exceptional",
+          count: 4084,
+          percent: 59.01,
+        },
+        {
+          id: 4,
+          title: "recommended",
+          count: 2270,
+          percent: 32.8,
+        },
+        {
+          id: 3,
+          title: "meh",
+          count: 440,
+          percent: 6.36,
+        },
+        {
+          id: 1,
+          title: "skip",
+          count: 127,
+          percent: 1.83,
+        },
+      ],
+    },
+    {
+      id: 3328,
+      slug: "the-witcher-3-wild-hunt",
+      name: "The Witcher 3: Wild Hunt",
+      released: "2015-05-18",
+      tba: false,
+      background_image:
+        "https://media.rawg.io/media/games/618/618c2031a07bbff6b4f611f10b6bcdbc.jpg",
+      rating: 4.65,
+      rating_top: 5,
+      ratings: [
+        {
+          id: 5,
+          title: "exceptional",
+          count: 5128,
+          percent: 77.1,
+        },
+        {
+          id: 4,
+          title: "recommended",
+          count: 1075,
+          percent: 16.16,
+        },
+        {
+          id: 3,
+          title: "meh",
+          count: 279,
+          percent: 4.19,
+        },
+        {
+          id: 1,
+          title: "skip",
+          count: 169,
+          percent: 2.54,
+        },
+      ],
+    },
+  ],
+};
