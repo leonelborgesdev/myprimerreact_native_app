d8cce08de26c897a8c1e7ecdcdfc07b2be1b7146
leonelborgesdev <leonel03t@gmail.com>
2024-06-04 13:32:15 -0400

    flatlist

--- src/components/RepositoryLIst.jsx
+++ src/components/RepositoryLIst.jsx
@@ -1,27 +1,17 @@
 import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, FlatList } from "react-native";
 import videogames from "../data/videogames.js";
 
 const ReposList = () => {
   return (
-    <View>
-      {videogames.results.map((game) => (
-        <View key={game.id}>
-          <Image style={styles.image} source={{ uri: game.background_image }} />
+    <FlatList
+      data={videogames.results}
+      renderItem={({ item: game }) => (
+        <View>
           <Text>{game.name}</Text>
-          <Text>Fecha de Lanzamiento: {game.released}</Text>
-          <Text>Rating: {game.rating}</Text>
-          <Text>
-            Generos:
-            {game.genres.map((genre) => (
-              <React.Fragment
-                key={genre.id}
-              >{` [${genre.name}]`}</React.Fragment>
-            ))}
-          </Text>
         </View>
-      ))}
-    </View>
+      )}
+    />
   );
 };
 
