607aee1d50a6cf3b12cacea8c93c73e439820dbe
leonelborgesdev <leonel03t@gmail.com>
2024-06-04 12:36:11 -0400

    videogames

--- src/components/RepositoryLIst.jsx
+++ src/components/RepositoryLIst.jsx
@@ -1,17 +1,36 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import videogames from "../data/videogames.js";
 
 const ReposList = () => {
   return (
     <View>
       {videogames.results.map((game) => (
-        <View>
+        <View key={game.id}>
+          <Image style={styles.image} source={{ uri: game.background_image }} />
           <Text>{game.name}</Text>
+          <Text>Fecha de Lanzamiento: {game.released}</Text>
+          <Text>Rating: {game.rating}</Text>
+          <Text>
+            Generos:
+            {game.genres.map((genre) => (
+              <React.Fragment
+                key={genre.id}
+              >{` [${genre.name}]`}</React.Fragment>
+            ))}
+          </Text>
         </View>
       ))}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    width: 150,
+    height: 150,
+    borderRadius: 4,
+  },
+});
 
 export default ReposList;
--- src/data/videogames.js
+++ src/data/videogames.js
@@ -13,6 +13,16 @@ export default {
         "https://media.rawg.io/media/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg",
       rating: 4.47,
       rating_top: 5,
+      genres: [
+        {
+          id: 4,
+          name: "Action",
+          slug: "action",
+          games_count: 179305,
+          image_background:
+            "https://media.rawg.io/media/games/021/021c4e21a1824d2526f925eff6324653.jpg",
+        },
+      ],
       ratings: [
         {
           id: 5,
@@ -50,6 +60,24 @@ export default {
         "https://media.rawg.io/media/games/618/618c2031a07bbff6b4f611f10b6bcdbc.jpg",
       rating: 4.65,
       rating_top: 5,
+      genres: [
+        {
+          id: 4,
+          name: "Action",
+          slug: "action",
+          games_count: 179305,
+          image_background:
+            "https://media.rawg.io/media/games/021/021c4e21a1824d2526f925eff6324653.jpg",
+        },
+        {
+          id: 5,
+          name: "RPG",
+          slug: "role-playing-games-rpg",
+          games_count: 55586,
+          image_background:
+            "https://media.rawg.io/media/games/f99/f9979698c43fd84c3ab69280576dd3af.jpg",
+        },
+      ],
       ratings: [
         {
           id: 5,
@@ -74,6 +102,163 @@ export default {
           title: "skip",
           count: 169,
           percent: 2.54,
+        },
+      ],
+    },
+    {
+      id: 4200,
+      slug: "portal-2",
+      name: "Portal 2",
+      released: "2011-04-18",
+      tba: false,
+      background_image:
+        "https://media.rawg.io/media/games/2ba/2bac0e87cf45e5b508f227d281c9252a.jpg",
+      rating: 4.61,
+      rating_top: 5,
+      genres: [
+        {
+          id: 2,
+          name: "Shooter",
+          slug: "shooter",
+          games_count: 59480,
+          image_background:
+            "https://media.rawg.io/media/games/c6b/c6bfece1daf8d06bc0a60632ac78e5bf.jpg",
+        },
+        {
+          id: 7,
+          name: "Puzzle",
+          slug: "puzzle",
+          games_count: 97279,
+          image_background:
+            "https://media.rawg.io/media/games/d03/d030347839f74454afcd1008248b08ae.jpg",
+        },
+      ],
+      ratings: [
+        {
+          id: 5,
+          title: "exceptional",
+          count: 3996,
+          percent: 70.14,
+        },
+        {
+          id: 4,
+          title: "recommended",
+          count: 1420,
+          percent: 24.93,
+        },
+        {
+          id: 3,
+          title: "meh",
+          count: 156,
+          percent: 2.74,
+        },
+        {
+          id: 1,
+          title: "skip",
+          count: 125,
+          percent: 2.19,
+        },
+      ],
+    },
+    {
+      id: 4291,
+      slug: "counter-strike-global-offensive",
+      name: "Counter-Strike: Global Offensive",
+      released: "2012-08-21",
+      tba: false,
+      background_image:
+        "https://media.rawg.io/media/games/736/73619bd336c894d6941d926bfd563946.jpg",
+      rating: 3.57,
+      rating_top: 4,
+      genres: [
+        {
+          id: 2,
+          name: "Shooter",
+          slug: "shooter",
+          games_count: 59474,
+          image_background:
+            "https://media.rawg.io/media/games/26d/26d4437715bee60138dab4a7c8c59c92.jpg",
+        },
+      ],
+      ratings: [
+        {
+          id: 4,
+          title: "recommended",
+          count: 1636,
+          percent: 46.8,
+        },
+        {
+          id: 3,
+          title: "meh",
+          count: 904,
+          percent: 25.86,
+        },
+        {
+          id: 5,
+          title: "exceptional",
+          count: 563,
+          percent: 16.1,
+        },
+        {
+          id: 1,
+          title: "skip",
+          count: 393,
+          percent: 11.24,
+        },
+      ],
+    },
+    {
+      id: 5286,
+      slug: "tomb-raider",
+      name: "Tomb Raider (2013)",
+      released: "2013-03-05",
+      tba: false,
+      background_image:
+        "https://media.rawg.io/media/games/021/021c4e21a1824d2526f925eff6324653.jpg",
+      rating: 4.05,
+      rating_top: 4,
+      genres: [
+        {
+          id: 4,
+          name: "Action",
+          slug: "action",
+          games_count: 179881,
+          image_background:
+            "https://media.rawg.io/media/games/4be/4be6a6ad0364751a96229c56bf69be59.jpg",
+        },
+        {
+          id: 6,
+          name: "Fighting",
+          slug: "fighting",
+          games_count: 11753,
+          image_background:
+            "https://media.rawg.io/media/games/aac/aac683272f862540a18625f02f5f3679.jpg",
+        },
+      ],
+      ratings: [
+        {
+          id: 4,
+          title: "recommended",
+          count: 2363,
+          percent: 60.5,
+        },
+        {
+          id: 5,
+          title: "exceptional",
+          count: 997,
+          percent: 25.52,
+        },
+        {
+          id: 3,
+          title: "meh",
+          count: 427,
+          percent: 10.93,
+        },
+        {
+          id: 1,
+          title: "skip",
+          count: 119,
+          percent: 3.05,
         },
       ],
     },
