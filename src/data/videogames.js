6f70bb4bb90eb4274c673d500910d16ab7b46cfc
leonelborgesdev <leonel03t@gmail.com>
2024-06-04 12:44:57 -0400

    platforms data

--- src/data/videogames.js
+++ src/data/videogames.js
@@ -13,6 +13,121 @@ export default {
         "https://media.rawg.io/media/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg",
       rating: 4.47,
       rating_top: 5,
+
+      platforms: [
+        {
+          platform: {
+            id: 4,
+            name: "PC",
+            slug: "pc",
+            image: null,
+            year_end: null,
+            year_start: null,
+            games_count: 528349,
+            image_background:
+              "https://media.rawg.io/media/games/120/1201a40e4364557b124392ee50317b99.jpg",
+          },
+          released_at: "2013-09-17",
+          requirements_en: {},
+          requirements_ru: null,
+        },
+        {
+          platform: {
+            id: 187,
+            name: "PlayStation 5",
+            slug: "playstation5",
+            image: null,
+            year_end: null,
+            year_start: 2020,
+            games_count: 1064,
+            image_background:
+              "https://media.rawg.io/media/games/559/559bc0768f656ad0c63c54b80a82d680.jpg",
+          },
+          released_at: "2013-09-17",
+          requirements_en: null,
+          requirements_ru: null,
+        },
+        {
+          platform: {
+            id: 186,
+            name: "Xbox Series S/X",
+            slug: "xbox-series-x",
+            image: null,
+            year_end: null,
+            year_start: 2020,
+            games_count: 922,
+            image_background:
+              "https://media.rawg.io/media/games/f87/f87457e8347484033cb34cde6101d08d.jpg",
+          },
+          released_at: "2013-09-17",
+          requirements_en: null,
+          requirements_ru: null,
+        },
+        {
+          platform: {
+            id: 18,
+            name: "PlayStation 4",
+            slug: "playstation4",
+            image: null,
+            year_end: null,
+            year_start: null,
+            games_count: 6805,
+            image_background:
+              "https://media.rawg.io/media/games/618/618c2031a07bbff6b4f611f10b6bcdbc.jpg",
+          },
+          released_at: "2013-09-17",
+          requirements_en: null,
+          requirements_ru: null,
+        },
+        {
+          platform: {
+            id: 16,
+            name: "PlayStation 3",
+            slug: "playstation3",
+            image: null,
+            year_end: null,
+            year_start: null,
+            games_count: 3165,
+            image_background:
+              "https://media.rawg.io/media/games/942/9424d6bb763dc38d9378b488603c87fa.jpg",
+          },
+          released_at: "2013-09-17",
+          requirements_en: null,
+          requirements_ru: null,
+        },
+        {
+          platform: {
+            id: 14,
+            name: "Xbox 360",
+            slug: "xbox360",
+            image: null,
+            year_end: null,
+            year_start: null,
+            games_count: 2799,
+            image_background:
+              "https://media.rawg.io/media/games/bc0/bc06a29ceac58652b684deefe7d56099.jpg",
+          },
+          released_at: "2013-09-17",
+          requirements_en: null,
+          requirements_ru: null,
+        },
+        {
+          platform: {
+            id: 1,
+            name: "Xbox One",
+            slug: "xbox-one",
+            image: null,
+            year_end: null,
+            year_start: null,
+            games_count: 5618,
+            image_background:
+              "https://media.rawg.io/media/games/310/3106b0e012271c5ffb16497b070be739.jpg",
+          },
+          released_at: "2013-09-17",
+          requirements_en: null,
+          requirements_ru: null,
+        },
+      ],
       genres: [
         {
           id: 4,
@@ -60,6 +175,120 @@ export default {
         "https://media.rawg.io/media/games/618/618c2031a07bbff6b4f611f10b6bcdbc.jpg",
       rating: 4.65,
       rating_top: 5,
+      platforms: [
+        {
+          platform: {
+            id: 186,
+            name: "Xbox Series S/X",
+            slug: "xbox-series-x",
+            image: null,
+            year_end: null,
+            year_start: 2020,
+            games_count: 922,
+            image_background:
+              "https://media.rawg.io/media/games/f87/f87457e8347484033cb34cde6101d08d.jpg",
+          },
+          released_at: "2015-05-18",
+          requirements_en: null,
+          requirements_ru: null,
+        },
+        {
+          platform: {
+            id: 187,
+            name: "PlayStation 5",
+            slug: "playstation5",
+            image: null,
+            year_end: null,
+            year_start: 2020,
+            games_count: 1064,
+            image_background:
+              "https://media.rawg.io/media/games/559/559bc0768f656ad0c63c54b80a82d680.jpg",
+          },
+          released_at: "2015-05-18",
+          requirements_en: null,
+          requirements_ru: null,
+        },
+        {
+          platform: {
+            id: 5,
+            name: "macOS",
+            slug: "macos",
+            image: null,
+            year_end: null,
+            year_start: null,
+            games_count: 103731,
+            image_background:
+              "https://media.rawg.io/media/games/157/15742f2f67eacff546738e1ab5c19d20.jpg",
+          },
+          released_at: "2015-05-18",
+          requirements_en: null,
+          requirements_ru: null,
+        },
+        {
+          platform: {
+            id: 18,
+            name: "PlayStation 4",
+            slug: "playstation4",
+            image: null,
+            year_end: null,
+            year_start: null,
+            games_count: 6805,
+            image_background:
+              "https://media.rawg.io/media/games/618/618c2031a07bbff6b4f611f10b6bcdbc.jpg",
+          },
+          released_at: "2015-05-18",
+          requirements_en: null,
+          requirements_ru: null,
+        },
+        {
+          platform: {
+            id: 7,
+            name: "Nintendo Switch",
+            slug: "nintendo-switch",
+            image: null,
+            year_end: null,
+            year_start: null,
+            games_count: 5448,
+            image_background:
+              "https://media.rawg.io/media/games/4cf/4cfc6b7f1850590a4634b08bfab308ab.jpg",
+          },
+          released_at: "2015-05-18",
+          requirements_en: null,
+          requirements_ru: null,
+        },
+        {
+          platform: {
+            id: 4,
+            name: "PC",
+            slug: "pc",
+            image: null,
+            year_end: null,
+            year_start: null,
+            games_count: 528349,
+            image_background:
+              "https://media.rawg.io/media/games/120/1201a40e4364557b124392ee50317b99.jpg",
+          },
+          released_at: "2015-05-18",
+          requirements_en: null,
+          requirements_ru: null,
+        },
+        {
+          platform: {
+            id: 1,
+            name: "Xbox One",
+            slug: "xbox-one",
+            image: null,
+            year_end: null,
+            year_start: null,
+            games_count: 5618,
+            image_background:
+              "https://media.rawg.io/media/games/310/3106b0e012271c5ffb16497b070be739.jpg",
+          },
+          released_at: "2015-05-18",
+          requirements_en: null,
+          requirements_ru: null,
+        },
+      ],
       genres: [
         {
           id: 4,
@@ -115,6 +344,109 @@ export default {
         "https://media.rawg.io/media/games/2ba/2bac0e87cf45e5b508f227d281c9252a.jpg",
       rating: 4.61,
       rating_top: 5,
+      platforms: [
+        {
+          platform: {
+            id: 16,
+            name: "PlayStation 3",
+            slug: "playstation3",
+            image: null,
+            year_end: null,
+            year_start: null,
+            games_count: 3165,
+            image_background:
+              "https://media.rawg.io/media/games/736/73619bd336c894d6941d926bfd563946.jpg",
+          },
+          released_at: "2011-04-18",
+          requirements_en: null,
+          requirements_ru: null,
+        },
+        {
+          platform: {
+            id: 4,
+            name: "PC",
+            slug: "pc",
+            image: null,
+            year_end: null,
+            year_start: null,
+            games_count: 530415,
+            image_background:
+              "https://media.rawg.io/media/games/8cc/8cce7c0e99dcc43d66c8efd42f9d03e3.jpg",
+          },
+          released_at: "2011-04-18",
+          requirements_en: null,
+          requirements_ru: {
+            minimum:
+              "Core 2 Duo/Athlon X2 2 ГГц,1 Гб памяти,GeForce 7600/Radeon X800,10 Гб на винчестере,интернет-соединение",
+            recommended:
+              "Core 2 Duo/Athlon X2 2.5 ГГц,2 Гб памяти,GeForce GTX 280/Radeon HD 2600,10 Гб на винчестере,интернет-соединение",
+          },
+        },
+        {
+          platform: {
+            id: 14,
+            name: "Xbox 360",
+            slug: "xbox360",
+            image: null,
+            year_end: null,
+            year_start: null,
+            games_count: 2801,
+            image_background:
+              "https://media.rawg.io/media/games/7cf/7cfc9220b401b7a300e409e539c9afd5.jpg",
+          },
+          released_at: "2011-04-18",
+          requirements_en: null,
+          requirements_ru: null,
+        },
+        {
+          platform: {
+            id: 6,
+            name: "Linux",
+            slug: "linux",
+            image: null,
+            year_end: null,
+            year_start: null,
+            games_count: 76948,
+            image_background:
+              "https://media.rawg.io/media/games/6fc/6fcf4cd3b17c288821388e6085bb0fc9.jpg",
+          },
+          released_at: "2011-04-18",
+          requirements_en: null,
+          requirements_ru: null,
+        },
+        {
+          platform: {
+            id: 5,
+            name: "macOS",
+            slug: "macos",
+            image: null,
+            year_end: null,
+            year_start: null,
+            games_count: 104007,
+            image_background:
+              "https://media.rawg.io/media/games/b7b/b7b8381707152afc7d91f5d95de70e39.jpg",
+          },
+          released_at: "2011-04-18",
+          requirements_en: null,
+          requirements_ru: null,
+        },
+        {
+          platform: {
+            id: 1,
+            name: "Xbox One",
+            slug: "xbox-one",
+            image: null,
+            year_end: null,
+            year_start: null,
+            games_count: 5622,
+            image_background:
+              "https://media.rawg.io/media/games/960/960b601d9541cec776c5fa42a00bf6c4.jpg",
+          },
+          released_at: "2011-04-18",
+          requirements_en: null,
+          requirements_ru: null,
+        },
+      ],
       genres: [
         {
           id: 2,
@@ -170,6 +502,80 @@ export default {
         "https://media.rawg.io/media/games/736/73619bd336c894d6941d926bfd563946.jpg",
       rating: 3.57,
       rating_top: 4,
+      platforms: [
+        {
+          platform: {
+            id: 4,
+            name: "PC",
+            slug: "pc",
+            image: null,
+            year_end: null,
+            year_start: null,
+            games_count: 528349,
+            image_background:
+              "https://media.rawg.io/media/games/120/1201a40e4364557b124392ee50317b99.jpg",
+          },
+          released_at: "2012-08-21",
+          requirements_en: {
+            minimum:
+              '<strong>Minimum:</strong><br><ul class="bb_ul"><li><strong>OS:</strong> Windows® 7/Vista/XP<br></li><li><strong>Processor:</strong> Intel® Core™ 2 Duo E6600 or AMD Phenom™ X3 8750 processor or better<br></li><li><strong>Memory:</strong> 2 GB RAM<br></li><li><strong>Graphics:</strong> Video card must be 256 MB or more and should be a DirectX 9-compatible with support for Pixel Shader 3.0<br></li><li><strong>DirectX:</strong> Version 9.0c<br></li><li><strong>Storage:</strong> 15 GB available space</li></ul>',
+          },
+          requirements_ru: {
+            minimum:
+              "Сore 2 Duo/Athlon x2 64 1.8 ГГц,2 Гб памяти,GeForce 8800/Radeon X9800,7.6 Гб на винчестере,интернет-соединение",
+            recommended:
+              "Core 2 Duo E6600/Phenom X3 8750,4 Гб памяти,GeForce 480 GTX/Radeon HD 6970,7.6 Гб на винчестере,интернет-соединение",
+          },
+        },
+        {
+          platform: {
+            id: 6,
+            name: "Linux",
+            slug: "linux",
+            image: null,
+            year_end: null,
+            year_start: null,
+            games_count: 76731,
+            image_background:
+              "https://media.rawg.io/media/games/9fa/9fa63622543e5d4f6d99aa9d73b043de.jpg",
+          },
+          released_at: "2012-08-21",
+          requirements_en: null,
+          requirements_ru: null,
+        },
+        {
+          platform: {
+            id: 14,
+            name: "Xbox 360",
+            slug: "xbox360",
+            image: null,
+            year_end: null,
+            year_start: null,
+            games_count: 2799,
+            image_background:
+              "https://media.rawg.io/media/games/bc0/bc06a29ceac58652b684deefe7d56099.jpg",
+          },
+          released_at: "2012-08-21",
+          requirements_en: null,
+          requirements_ru: null,
+        },
+        {
+          platform: {
+            id: 16,
+            name: "PlayStation 3",
+            slug: "playstation3",
+            image: null,
+            year_end: null,
+            year_start: null,
+            games_count: 3165,
+            image_background:
+              "https://media.rawg.io/media/games/942/9424d6bb763dc38d9378b488603c87fa.jpg",
+          },
+          released_at: "2012-08-21",
+          requirements_en: null,
+          requirements_ru: null,
+        },
+      ],
       genres: [
         {
           id: 2,
@@ -217,6 +623,113 @@ export default {
         "https://media.rawg.io/media/games/021/021c4e21a1824d2526f925eff6324653.jpg",
       rating: 4.05,
       rating_top: 4,
+      platforms: [
+        {
+          platform: {
+            id: 18,
+            name: "PlayStation 4",
+            slug: "playstation4",
+            image: null,
+            year_end: null,
+            year_start: null,
+            games_count: 6808,
+            image_background:
+              "https://media.rawg.io/media/games/942/9424d6bb763dc38d9378b488603c87fa.jpg",
+          },
+          released_at: "2013-03-05",
+          requirements_en: null,
+          requirements_ru: null,
+        },
+        {
+          platform: {
+            id: 16,
+            name: "PlayStation 3",
+            slug: "playstation3",
+            image: null,
+            year_end: null,
+            year_start: null,
+            games_count: 3164,
+            image_background:
+              "https://media.rawg.io/media/games/490/49016e06ae2103881ff6373248843069.jpg",
+          },
+          released_at: "2013-03-05",
+          requirements_en: null,
+          requirements_ru: null,
+        },
+        {
+          platform: {
+            id: 14,
+            name: "Xbox 360",
+            slug: "xbox360",
+            image: null,
+            year_end: null,
+            year_start: null,
+            games_count: 2799,
+            image_background:
+              "https://media.rawg.io/media/games/49c/49c3dfa4ce2f6f140cc4825868e858cb.jpg",
+          },
+          released_at: "2013-03-05",
+          requirements_en: null,
+          requirements_ru: null,
+        },
+        {
+          platform: {
+            id: 5,
+            name: "macOS",
+            slug: "macos",
+            image: null,
+            year_end: null,
+            year_start: null,
+            games_count: 103919,
+            image_background:
+              "https://media.rawg.io/media/games/942/9424d6bb763dc38d9378b488603c87fa.jpg",
+          },
+          released_at: "2013-03-05",
+          requirements_en: {
+            minimum:
+              "Minimum:\r\n\r\nOS: macOS 10.9.1\r\n\r\nProcessor: 2.0GHz Intel or greater\r\n\r\nMemory: 4GB\r\n\r\nGraphics: 512Mb AMD 4850, 512Mb Nvidia 650M, Intel HD4000\r\n\r\nHard Drive: 14GB",
+          },
+          requirements_ru: null,
+        },
+        {
+          platform: {
+            id: 4,
+            name: "PC",
+            slug: "pc",
+            image: null,
+            year_end: null,
+            year_start: null,
+            games_count: 529798,
+            image_background:
+              "https://media.rawg.io/media/games/021/021c4e21a1824d2526f925eff6324653.jpg",
+          },
+          released_at: "2013-03-05",
+          requirements_en: {
+            minimum:
+              '<strong>Minimum:</strong><br><ul class="bb_ul"><li><strong>OS:</strong>Windows XP / Windows Vista / Windows 7<br>\t</li><li><strong>Processor:</strong>1.8 GHz Processor<br>\t</li><li><strong>Memory:</strong>512 MB RAM<br>\t</li><li><strong>Graphics:</strong>3D graphics card compatible with DirectX 9<br>\t</li><li><strong>DirectX®:</strong>9.0<br>\t</li><li><strong>Hard Drive:</strong>2 GB HD space</li></ul>',
+          },
+          requirements_ru: {
+            minimum: "i486-100, 8 Мб",
+            recommended: "Pentium 166, 16 Мб",
+          },
+        },
+        {
+          platform: {
+            id: 1,
+            name: "Xbox One",
+            slug: "xbox-one",
+            image: null,
+            year_end: null,
+            year_start: null,
+            games_count: 5621,
+            image_background:
+              "https://media.rawg.io/media/games/4cf/4cfc6b7f1850590a4634b08bfab308ab.jpg",
+          },
+          released_at: "2013-03-05",
+          requirements_en: null,
+          requirements_ru: null,
+        },
+      ],
       genres: [
         {
           id: 4,
