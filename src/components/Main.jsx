0096bfd6eae56699e5b00950b4c46ae5a66d2b7d
leonelborgesdev <leonel03t@gmail.com>
2024-06-04 11:50:30 -0400

    eslintconfig

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Constants from "expo-constants";
 import { Text, View } from "react-native";
 
 const Main = () => {
   return (
-    <View style={{ marginTop: Constants.sta }}>
+    // Constants te permite ponerle un margen superior al texto de forma que
+    // se ajuste al renderizar la vista en android o iOS
+    <View style={{ marginTop: Constants.statusBarHeight, flexGrow: 1 }}>
       <Text>VideoGame Repository</Text>
     </View>
   );
