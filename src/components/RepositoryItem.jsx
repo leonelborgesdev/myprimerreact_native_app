d3280f9fffd0999aa7ae5d9bee79b549c00944be
leonelborgesdev <leonel03t@gmail.com>
2024-06-05 12:05:59 -0400

    repository item

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,19 +1,25 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const RepositoryItem = (props) => (
-  <View
-    key={props.id}
-    style={{
-      padding: 20,
-      paddingBottom: 5,
-      paddingTop: 5,
-    }}
-  >
-    <Text style={{ fontWeight: "bold", marginBottom: 5 }}>{props.name}</Text>
+  <View key={props.id} style={styles.container}>
+    <Text style={styles.strong}>{props.name}</Text>
     <Text>Fecha de Lanzamiento: {props.released}</Text>
     <Text>Rating: {props.rating}</Text>
   </View>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    paddingBottom: 5,
+    paddingTop: 5,
+  },
+  strong: {
+    color: "#09f",
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+});
 
 export default RepositoryItem;
