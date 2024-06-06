ba364ee6a591326736275885e270f14f9966d4b1
leonelborgesdev <leonel03t@gmail.com>
2024-06-05 22:16:36 -0400

    platforms

--- src/components/RepositoryGenres.jsx
+++ src/components/RepositoryGenres.jsx
@@ -1,12 +1,21 @@
 import StyledText from "./StyledText.jsx";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 const RepositoryGenres = (props) => {
   return (
     <View style={{ flexDirection: "row", justifyContent: "space-around" }}>
+      <StyledText fontWeight="bold">Genres:</StyledText>
       <View>
         {props.genres.map((genre) => {
           return <StyledText key={genre.id}>{genre.name}</StyledText>;
+        })}
+      </View>
+      <StyledText fontWeight="bold">Platforms:</StyledText>
+      <View>
+        {props.platforms.map((obj) => {
+          return (
+            <StyledText key={obj.platform.id}>{obj.platform.name}</StyledText>
+          );
         })}
       </View>
     </View>
