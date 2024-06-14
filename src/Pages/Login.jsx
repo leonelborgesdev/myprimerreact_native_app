c1bd894c79ae222c8fb2028feeb2037b4c29cb42
leonelborgesdev <leonel03t@gmail.com>
2024-06-14 12:16:35 -0400

    submit

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Formik } from "formik";
-import { Text, TextInput, View } from "react-native";
+import { Button, Text, TextInput, View } from "react-native";
 
 const initialValues = {
   email: "",
@@ -13,11 +13,20 @@ const Login = () => {
       initialValues={initialValues}
       onSubmit={(values) => console.log(values)}
     >
-      {() => {
+      {({ handleChange, handleSubmit, values }) => {
         return (
           <View>
-            <TextInput />
-            <TextInput />
+            <TextInput
+              placeholder="E-mail"
+              value={values.email}
+              onChangeText={handleChange("email")}
+            />
+            <TextInput
+              placeholder="Password"
+              value={values.password}
+              onChangeText={handleChange("password")}
+            />
+            <Button onPress={handleSubmit} title="Log In" />
           </View>
         );
       }}
