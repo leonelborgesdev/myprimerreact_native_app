c1a672c17d9d066769deb647d4557b08e5d72e29
leonelborgesdev <leonel03t@gmail.com>
2024-06-14 11:58:26 -0400

    initialValues

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { Formik } from "formik";
 import { Text } from "react-native";
+
+const initialValues = {
+  email: "",
+  password: "",
+};
 
 const Login = () => {
   return <Text>Login</Text>;
